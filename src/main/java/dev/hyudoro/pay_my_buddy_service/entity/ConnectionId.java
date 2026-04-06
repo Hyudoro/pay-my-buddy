36f60dd4188c0a326080c89084f9d82a7c01cf14
Hyudoro <jh.virtual.tech@gmail.com>
2026-04-06 17:30:33 +0200

    feat(entity): add ConnectionId embeddable composite PK

---
+++ src/main/java/dev/hyudoro/pay_my_buddy_service/entity/ConnectionId.java
@@ -0,0 +1,44 @@
+package dev.hyudoro.pay_my_buddy_service.entity;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+
+
+@Embeddable
+public class ConnectionId implements Serializable{
+
+    @Column(name = "user_id", columnDefinition = "uuid", nullable = false, updatable = false)
+    private UUID userId;
+
+    @Column(name = "connection_user_id", columnDefinition = "uuid", nullable = false, updatable = false)
+    private UUID connectionUserId;
+
+    protected ConnectionId(){} // Empty constructor for JPA (only for compliance avoiding throws at runtime)
+
+    public ConnectionId(UUID userId, UUID connectionUserId){
+        this.userId = userId;
+        this.connectionUserId = connectionUserId;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this.equals(o)){ return true; }
+        if (!(o instanceof ConnectionId that)){ return false; }
+
+        return Objects.equals(userId, that.userId) && Objects.equals(connectionUserId, that.connectionUserId);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, connectionUserId);
+    }
+
+    public UUID getUserId() { return userId; }
+    public UUID getConnectionUserId() { return userId; }
+}
