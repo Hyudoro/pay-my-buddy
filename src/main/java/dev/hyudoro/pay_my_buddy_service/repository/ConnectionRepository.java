3080e22d884c0c20f3f2dbdeed42241967485f86
Hyudoro <jh.virtual.tech@gmail.com>
2026-04-08 21:47:15 +0200

    feat(repository): add ConnectionRepository with symmetric connection query

--- src/main/java/dev/hyudoro/pay_my_buddy_service/repository/ConnectionRepository.java
+++ src/main/java/dev/hyudoro/pay_my_buddy_service/repository/ConnectionRepository.java
@@ -1,10 +1,33 @@
 package dev.hyudoro.pay_my_buddy_service.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import dev.hyudoro.pay_my_buddy_service.entity.Connection;
 import dev.hyudoro.pay_my_buddy_service.entity.ConnectionId;
+import dev.hyudoro.pay_my_buddy_service.entity.User;
 
 public interface ConnectionRepository extends JpaRepository<Connection, ConnectionId>{
+
+    @Query("""
+           SELECT
+            CASE
+             WHEN c.user.id = :userId
+              THEN
+                c.connectedUser
+              ELSE
+                c.user
+            END
+           FROM Connection c
+           WHERE c.user.id = :userId
+           OR c.connectedUser.id = :userId
+           """)
+    List<User> findConnectionsOf(@Param("userId") UUID userId);
+
 
 
 }
