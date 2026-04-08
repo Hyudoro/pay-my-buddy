697074479c978f9f908847146c7b390915b300f9
Hyudoro <jh.virtual.tech@gmail.com>
2026-04-08 23:59:38 +0200

    feat(repository): add TransactionRepository with paginated transaction query

--- src/main/java/dev/hyudoro/pay_my_buddy_service/repository/TransactionRepository.java
+++ src/main/java/dev/hyudoro/pay_my_buddy_service/repository/TransactionRepository.java
@@ -1,8 +1,25 @@
 package dev.hyudoro.pay_my_buddy_service.repository;
 
+import org.springframework.data.domain.Pageable;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import dev.hyudoro.pay_my_buddy_service.entity.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
+
+    @Query("""
+           SELECT t
+           FROM Transaction t
+           WHERE t.sender.id = :userId OR t.receiver.id = :userId
+           ORDER BY t.createdAt DESC
+        """)
+    Page<Transaction> findTransactionsOf(@Param("userId") UUID userId, Pageable pageable);
+
+
 }
