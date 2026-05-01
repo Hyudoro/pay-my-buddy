f30d5abff8977fef66a6b880cb14a8ee8fd772b2
Hyudoro <jh.virtual.tech@gmail.com>
2026-05-01 22:15:46 +0200

    feat(profile): add profile password update mvc enpoint

--- src/main/java/dev/hyudoro/pay_my_buddy_service/controller/ProfileViewController.java
+++ src/main/java/dev/hyudoro/pay_my_buddy_service/controller/ProfileViewController.java
@@ -1,14 +1,17 @@
 package dev.hyudoro.pay_my_buddy_service.controller;
 
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import dev.hyudoro.pay_my_buddy_service.dto.ProfileUpdatePasswordRequest;
 import dev.hyudoro.pay_my_buddy_service.dto.ProfileUpdateRequest;
 import dev.hyudoro.pay_my_buddy_service.service.inter.ProfileService;
+import jakarta.validation.Valid;
 
 
 @Controller
@@ -35,6 +38,26 @@ public class ProfileViewController{
         }
         return "redirect:/profile";
     }
+
+    @PostMapping("/profile/password")
+    public String updateUserPassword(@Valid @ModelAttribute ProfileUpdatePasswordRequest request,
+                                     BindingResult bindingResult,
+                                     RedirectAttributes redirectAttributes){
+        if(bindingResult.hasErrors()){
+            String errorMessage = bindingResult.getAllErrors()
+                .getFirst()
+                .getDefaultMessage();
+            redirectAttributes.addFlashAttribute("error",errorMessage);
+            return "redirect:/profile";
+        }
+        try{
+            profileService.updatePassword(request);
+        }catch(Exception e){
+            redirectAttributes.addFlashAttribute("error",e.getMessage());
+        }
+        return "redirect:/profile";
+    }
+
 
 
 }
